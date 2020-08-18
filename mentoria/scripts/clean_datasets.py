674a70f7c253c8a23d49f354edda4b809e0e06c5
Leo Palacios <palaciosleo@gmail.com>
2020-08-18 18:37:41 -0300

    Changed protocol version of pkl for compatibility reasons.

--- mentoria/scripts/clean_datasets.py
+++ mentoria/scripts/clean_datasets.py
@@ -115,7 +115,7 @@ def main():
                                                'rdo_ri_geo', 'marca_depurada', 'marca_dummy', 'region', 'relat_cuartil_25',
                                                'relat_cuartil_75', 'rdo_ri_geo_relativo'], inplace=True)
 
-        pd.to_pickle(precio_sucursal_producto, '../models/precio_sucursal_producto.pkl', compression="zip", protocol=4)
+        pd.to_pickle(precio_sucursal_producto, '../models/precio_sucursal_producto.pkl', compression="zip")
 
     except Exception as e:
         logger.error('%s | %s', 'main', str(e))
