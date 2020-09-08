89c15a3f86ec53dbdf1d327a779ae9acc63eecd1
Leo Palacios <palaciosleo@gmail.com>
2020-09-08 12:44:08 -0300

    Added protocol 4 for train/eval

--- mentoria/scripts/split_dataset.py
+++ mentoria/scripts/split_dataset.py
@@ -13,7 +13,7 @@ full_ds = shuffle(full_ds)
 n_split = 20
 train_ratio = 0.7
 ds_len = len(full_ds)
-fold = int(ds_len / 20)
+fold = int(ds_len / n_split)
 
 
 i = 0
@@ -21,9 +21,9 @@ for n in range(0, n_split):
     j = i + fold
     curr_ratio = i / ds_len
     if curr_ratio <= train_ratio:
-        pd.to_pickle(full_ds.iloc[i:j, :], '../models/psp_train_{}.pkl'.format(n), compression="zip")
+        pd.to_pickle(full_ds.iloc[i:j, :], '../models/psp_train_{}.pkl'.format(n), compression="zip", protocol=4)
     else:
-        pd.to_pickle(full_ds.iloc[i:, :], '../models/psp_eval.pkl', compression="zip")
+        pd.to_pickle(full_ds.iloc[i:, :], '../models/psp_eval.pkl', compression="zip", protocol=4)
         break
     i = j
 
